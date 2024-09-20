306e7ba46166b108d97ece3f11b06f0fe577f6e5
BlueDevDreamy <abc.dreamy.618@gmail.com>
2024-09-19 20:22:53 -0700

    Updated images and description.

--- data/index.ts
+++ data/index.ts
@@ -72,35 +72,19 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    title: "Human Resources Management System",
+    des: "Explore talents and manage resources using effeciant Spring boot backend system.",
     img: "/p1.svg",
     iconLists: ["/java.svg", "/spring.svg", "/mysql.svg", "/re.svg"],
-    link: "/ui.earth.com",
+    link: "https://tadagberhplus.com/connexion/",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    title: "E3Buildings Science - Buildings Inspection App",
+    des: "Simplify managing inspections of buildings, builders, communities anytime, anywhere.",
     img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "/ui.yoom.com",
-  },
-  {
-    id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "/ui.aiimg.com",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "/ui.apple.com",
+    iconLists: ["/kotlin.svg", "/compose.svg", "/django.svg"],
+    link: null,
   },
 ];
 
@@ -173,29 +157,29 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Software Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using Java and React.js, enhancing interactivity.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Junior Developer - Bridgit",
+    desc: "Developed and maintained software solutions, primarily using Java.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Developer, Tester, Inspector - Swipe",
+    desc: "Tested and inspected software systems for accuracy and reliability while developing enterprise backend project.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Lead Software Engineer",
+    desc: "Led and managed Android teams, ensuring timely delivery and high-quality outcomes.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
